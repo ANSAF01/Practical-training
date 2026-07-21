7a8af82f9f18c6f09e77e082707afab7835e14f8
Ansaf <mohammedansaf01@gmail.com>
2026-07-21 20:30:03 +0530

    Reverse 1–10 using generator functions

---
+++ js/reverseGenerator.js
@@ -0,0 +1,19 @@
+// Reverse 1–10 using generator functions
+
+function* reverseGenerator(arr) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        yield arr[i]
+    }
+}
+let gen = reverseGenerator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
