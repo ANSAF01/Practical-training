f6401016cf9a9f15097f739e965922616cce2038
Ansaf <mohammedansaf01@gmail.com>
2026-09-23 15:01:52 +0530

    twoSum

--- js/twoSum.js
+++ js/twoSum.js
@@ -1,3 +1,6 @@
+// Problem: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// Hash Table Approach: Store each number and its index in a hash map as you iterate. For each number, check if its complement (target - currentNumber) already exists in the hash map.
 function twoSum(nums, target) {
   const map = new Map();
   for (let i = 0; i < nums.length; i++) {
