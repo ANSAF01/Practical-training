481d2ffae9860b977a770a5486dda42031dbb244
Ansaf <mohammedansaf01@gmail.com>
2026-09-24 22:05:32 +0530

    bubble Sort

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -12,5 +12,8 @@ function bubbleSort(arr) {
   return arr;
 }
 
-let arr = [5, 3, 4, 1, 2];
-console.log(bubbleSort(arr));
+console.log(bubbleSort([5, 2, 9, 1, 5, 6])); // [1, 2, 5, 5, 6, 9]
+
+console.log(bubbleSort([10, 8, 6, 4, 2])); // [2, 4, 6, 8, 10]
+
+console.log(bubbleSort([-3, 2, -1, 0, 5])); // [-3, -1, 0, 2, 5]
